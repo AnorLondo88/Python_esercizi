25b75041bf99a8f394fb5c417539c1e129deb21a
AnorLondo88 <alessandro.cugnidoro@outlook.com>
2025-10-15 18:00:11 +0200

    add files

---
+++ casting.py
@@ -0,0 +1,11 @@
+#Chiedere all'utente di inserire un numero intero  
+num_int=int(input("inserisci un numero intero "))
+
+#convertirlo in float, stamparlo
+num_float=float(num_int)
+print(num_float)
+
+#poi convertirlo in stringa e stamparlo con un messaggio
+num_str=str(num_float)
+print("il numero è",num_str)
+
---
+++ variabili.py
@@ -0,0 +1,13 @@
+#Crea una variabile con il tuo nome e stampala.
+nome=("alessandro")
+print("il tuo nome è", nome)
+
+#Chiedi all'utente la sua età e stampala raddoppiata.
+eta=int(input("quanti anni hai?"))
+print("hai ben",eta *2 ,"anni")
+
+#Crea due numeri e stampa la loro somma.
+n1=27
+n2=58
+print(n1+n2)
+
